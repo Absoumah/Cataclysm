cbc8b0e5c7db8f194aa5d750ec1d35a3351f291e
Soumah Abdoulaye <abdoulaye.soumah@etu.univ-orleans.fr>
2023-03-13 22:00:33 +0100

    first commit

--- Abdoulaye-Soumah_Abdoulaye-Barry_Charles-Diaw/IA/IAForet/FairePeur.java
+++
@@ -1,8 +0,0 @@
-package Modele;
-
-class FairePeur implements StrategieRenard {
-    @Override
-    public void execute(String[][] matrice, int i, int i1) {
-        //System.out.println("Faire peur");
-    }
-}
